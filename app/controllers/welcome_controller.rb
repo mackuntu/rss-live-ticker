06ea3d60bd90c0ca6a49726e325502a091f3abbd
Martin Qian <mackuntu@gmail.com>
2013-09-29 11:20:36 -0700

    1 min mock rss feed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
+require 'feedzirra'
+
 class WelcomeController < ApplicationController
   def index
+    feed = Feedzirra::Feed.fetch_and_parse("http://online.wsj.com/xml/rss/3_7455.xml")
+    @entries = feed.entries
   end
 end
